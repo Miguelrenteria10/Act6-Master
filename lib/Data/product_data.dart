9a22c52d1b380ad9ece886dfd819228f143bdbe6
MIGUEL_JARED_RENTERIA_MARRUFO <a.21308051281099@cbtis128.edu.mx>
2024-04-26 15:28:31 +0000

    Actualizado

---
+++ lib/Data/product_data.dart
@@ -0,0 +1,16 @@
+import 'package:Renteria1099/modelos/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+      img: "assets/images/image1.png",
+      title: "Productos De Cocacola",
+      description: "Es una de nustra especialidad"),
+  ProductModel(
+      img: "assets/images/image2.jpg",
+      title: "Cocacola De Vidrio",
+      description: "Una de los mejores refrescos de toda la ciudad"),
+  ProductModel(
+      img: "assets/images/image3.png",
+      title: "Cocacola De Lata",
+      description: "Uno de los mejores refrescos de toda la ciudad"),
+];
